a6c6069ad7b7d2a24effff11b3a8d49ed4621bae
Nacho Vera <nachodox@gmail.com>
2023-06-13 20:47:57 -0400

    variables declaration

---
+++ infrastructure/dev/variables.tf
@@ -0,0 +1,16 @@
+variable "host_os" {
+  type    = string
+  default = "windows"
+}
+
+variable "git_user" {
+  type = string
+}
+
+variable "git_pass" {
+  type = string
+}
+
+variable "ktf_git_repository" {
+  type = string
+}
