a3c173c865805f15d456690b7d746efb648644ad
Nacho Vera <nachodox@gmail.com>
2023-06-15 23:02:34 -0400

    shared project with basic resources

---
+++ code/Shared/MapSnapshot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KillTheFly.Shared
+{
+    public class MapSnapshot
+    {
+        public int Foes { get; set; }
+        public int GameTime { get; set; }
+        public int MilisecondsPast { get; set; }
+        public IEnumerable<MapRepresentation> Map { get; set; } = new List<MapRepresentation>();
+    }
+}
