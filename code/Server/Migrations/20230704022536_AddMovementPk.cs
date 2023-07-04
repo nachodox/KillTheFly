678fe248e4f1c8e098aa6a3b177673566555f984
Nacho Vera <nachodox@gmail.com>
2023-07-03 22:52:58 -0400

    initial migrations

---
+++ code/Server/Migrations/20230704022536_AddMovementPk.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KillTheFly.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMovementPk : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Guid",
+                table: "Kills",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Movements",
+                table: "Movements",
+                column: "Guid");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Kills",
+                table: "Kills",
+                column: "Guid");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Movements",
+                table: "Movements");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Kills",
+                table: "Kills");
+
+            migrationBuilder.DropColumn(
+                name: "Guid",
+                table: "Kills");
+        }
+    }
+}
