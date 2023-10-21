bd693b8c5fd73d17934258fad7007645b187c0f5
Ubuntu <adminuser@ktf-vm.rsbvlpkwpbpuzka440v0t20ize.bx.internal.cloudapp.net>
2023-10-21 21:36:04 +0000

    database add images

--- code/Server/Migrations/20230930212619_Images.Designer.cs
+++ code/Server/Migrations/20230930212619_Images.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -10,9 +11,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace KillTheFly.Server.Migrations
 {
     [DbContext(typeof(KTFDatabaseContext))]
-    partial class KTFDatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20230930212619_Images")]
+    partial class Images
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
